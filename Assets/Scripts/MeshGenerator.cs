e426eb449553c3388f232f3d33729ca12b1e75e0
Haldor Johansen <haldorjh@gmail.com>
2023-09-16 10:06:48 +0200

    Ball sliding correctly along plane

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -2,18 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class MeshGenerator : MonoBehaviour
 {
-    Mesh mesh;
+    private Mesh _mesh;
 
     public Vector3[] vertices;
     public int[] triangles;
 
     void Start()
     {
-        mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
+        _mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = _mesh;
 
         CreateShape();
         UpdateMesh();
@@ -42,6 +43,37 @@ public class MeshGenerator : MonoBehaviour
         }
         return 0.0f;
     }
+
+    void CreateShape()
+    {
+        vertices = new Vector3[]
+        {
+            new(0.0f, 0.097f, 0.0f),
+            new(0.4f, 0.005f, 0.0f),
+            new(0.0f, 0.005f, 0.4f),
+            new(0.4f, 0.075f, 0.4f),
+            new(0.8f, 0.007f, 0.4f),
+            new(0.8f, 0.039f, 0.0f)
+        };
+
+        triangles = new int[]
+        {
+            2, 1, 0
+            // 2, 3, 1,
+            // 4, 1, 3,
+            // 1, 4, 5
+        };
+    }
+
+    void UpdateMesh()
+    {
+        _mesh.Clear();
+
+        _mesh.vertices = vertices;
+        _mesh.triangles = triangles;
+        _mesh.RecalculateNormals();
+    }
+    
     public static Vector3 BarycentricCoordinates(Vector2 p1, Vector2 p2, Vector2 p3, Vector2 pt)
     {
         Vector2 p12 = p2 - p1;
@@ -66,35 +98,5 @@ public class MeshGenerator : MonoBehaviour
         baryc.z = n.y / areal123;
 
         return baryc;
-    }
-
-    void CreateShape()
-    {
-        vertices = new Vector3[]
-        {
-            new Vector3(0.0f, 0.097f, 0.0f),
-            new Vector3(0.4f, 0.005f, 0.0f),
-            new Vector3(0.0f, 0.005f, 0.4f),
-            new Vector3(0.4f, 0.075f, 0.4f),
-            new Vector3(0.8f, 0.007f, 0.4f),
-            new Vector3(0.8f, 0.039f, 0.0f)
-        };
-
-        triangles = new int[]
-        {
-            2, 1, 0
-            // 2, 3, 1,
-            // 4, 1, 3,
-            // 1, 4, 5
-        };
-    }
-
-    void UpdateMesh()
-    {
-        mesh.Clear();
-
-        mesh.vertices = vertices;
-        mesh.triangles = triangles;
-        mesh.RecalculateNormals();
     }
 }
--- Assets/Scripts/RollingBall.cs
+++ Assets/Scripts/RollingBall.cs
@@ -7,24 +7,32 @@ using UnityEngine.Serialization;
 
 public class RollingBall : MonoBehaviour
 {
-    private float _radius = 0.016f;
+    private float _radius = 0.015f;
     public MeshGenerator meshGenerator;
 
+    public float mass = 1.0f;
+    
     private Vector3 _currentVelocity;
     private Vector3 _previousVelocity;
     
-    private Vector3 _currentPos = new Vector3(0.05f, 0.1f, 0.05f);
-    private Vector3 _previousPos; 
+    private Vector3 _currentPos;
+    private Vector3 _previousPos;
 
     private void Awake()
     {
-        //meshGenerator = GetComponent<MeshGenerator>();
-        transform.position = new Vector3(0.01f, 0.1f, 0.01f);
+        _currentPos = new Vector3(0.0f, 0.097f + _radius, 0.0f);
+        _previousPos = _currentPos;
     }
 
     void FixedUpdate()
     {
-        Move();
+        
+        if (meshGenerator)
+            Move();
+        else
+        {
+            FreeFall();
+        }
     }
     
     void Move()
@@ -36,9 +44,10 @@ public class RollingBall : MonoBehaviour
             Vector3 p0 = meshGenerator.vertices[meshGenerator.triangles[i]];
             Vector3 p1 = meshGenerator.vertices[meshGenerator.triangles[i + 1]];
             Vector3 p2 = meshGenerator.vertices[meshGenerator.triangles[i + 2]];
+            
             // Find the balls position in the xz-plane
-            var position = transform.position;
-            Vector2 pos = new Vector2(position.x, position.z);
+            Vector2 pos = new Vector2(_currentPos.x, _currentPos.z);
+            
             // Find which triangle the ball is currently on with barycentric coordinates
             Vector3 baryCoords = MeshGenerator.BarycentricCoordinates(
                 new Vector2(p0.x, p0.z), 
@@ -46,24 +55,44 @@ public class RollingBall : MonoBehaviour
                 new Vector2(p2.x, p2.z),  
                 pos
                 );
+            
             if (baryCoords is { x: >= 0.0f, y: >= 0.0f, z: >= 0.0f })
             {
                 // Calculate normal vector
                 Vector3 n = Vector3.Cross(p1 - p0, p2 - p0).normalized;
                 // Calculate acceleration vector
                 Vector3 acceleration = new Vector3(n.x * n.y, n.y * n.y - 1, n.z * n.y) * -Physics.gravity.y;
+                
                 // Update velocity
-                _currentVelocity = _previousVelocity + acceleration * Time.deltaTime;
+                _currentVelocity = _previousVelocity + acceleration * Time.fixedDeltaTime;
                 _previousVelocity = _currentVelocity;
                 // Update position
-                _currentPos = _previousPos + _currentVelocity * Time.deltaTime;
+                _currentPos = _previousPos + _currentVelocity * Time.fixedDeltaTime;
                 _previousPos = _currentPos;
                 
                 transform.position = _currentPos;
+                
+                Debug.Log(_currentPos);
+            }
+            else
+            {
+                FreeFall();
             }
         }
     }
 
+    void FreeFall()
+    {
+        // Update velocity
+        _currentVelocity = _previousVelocity + Physics.gravity * Time.fixedDeltaTime;
+        _previousVelocity = _currentVelocity;
+        // Update position
+        _currentPos = _previousPos + _currentVelocity * Time.fixedDeltaTime;
+        _previousPos = _currentPos;
+                
+        transform.position = _currentPos;
+    }
+    
     void OnDrawGizmosSelected()
     {
         // Draw a yellow sphere at the transform's position
